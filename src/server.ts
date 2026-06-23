3352f76e461659cbe4322ea5979b46a98d091e1a
nishat jahan <nishatjahanposhpa@gmail.com>
2026-06-23 13:21:57 +0600

    feat:explored  Route Handling with HTTP Requests & Responses and practice  in server.js

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,28 @@
-import { createServer, IncomingMessage, Server } from "node:http";
+import { createServer, IncomingMessage, Server, ServerResponse } from "node:http";
 
 const server : Server = createServer(
-    (req:IncomingMessage,res)=>{
-        console.log(req)
+    (req:IncomingMessage,res:ServerResponse)=>{
+    //    console.log(req.url);
+    //     console.log(req.method);
+
+    
+const url =req.url
+const method= req.method
+
+if ( url ==="/" && method==="GET") {
+    // res.writeHead(200 ,{ "content-type" :" text/plain"})
+    res.writeHead(200 ,{ "content-type" :" application/json"})
+    // res.end({massage: `Node Server is running on port ${PORT}`}) // server will crash .. have to send with stringify
+    res.end(JSON.stringify({massage: `Node Server is running on port ${PORT}`}))
+
+} else if (url==="/users" && method==="GET"){
+    res.writeHead(200 ,{ "content-type" :" application/json"})
+   res.end(JSON.stringify({massage: "this is User Route"}))
+} else 
+    // res.writeHead(404 ,{ "content-type" :" text/plain"})
+res.writeHead(404 ,{ "content-type" :" application/json"})
+       res.end(JSON.stringify({massage:"Route not found"}))
+
     }
 )
 
@@ -11,3 +31,4 @@ const PORT =5000
 server.listen(5000,()=>{
     console.log(`Server is running on the port ${PORT}`)
 })
+
